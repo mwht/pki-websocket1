02cc65af478c1f81bfff8961a94f3d16cdbea861
Sebastian Madejski <209388@edu.p.lodz.pl>
2019-03-22 09:19:25 +0100

    add storing room ID in WebSocket session

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -2,14 +2,16 @@ import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 
-@ServerEndpoint("/broadcast")
-public class BroadcastServer {
+@ServerEndpoint("/room/{roomid}")
+public class ChatServer {
 
     @OnOpen
-    public void onOpen(Session session) {
+    public void onOpen(Session session, @PathParam("roomid") String roomid) {
+        session.getUserProperties().put("roomID", roomid);
         SessionHandler.addSession(session);
         System.out.println(session.getId() + " has established the connection.");
         SessionHandler.sendToSession(session, "Connection established.");
